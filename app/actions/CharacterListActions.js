ef5ad4554e2502bfcb58bfecc59db873ac036888
jssPolls <jagadguru6969@gmail.com>
2017-06-27 23:36:41 +0530

    Update CharacterListActions.js

--- app/actions/CharacterListActions.js
+++ app/actions/CharacterListActions.js
@@ -15,15 +15,15 @@ class CharacterListActions {
       bloodline: payload.bloodline
     };
 
-    if (payload.category === 'Pop') {
+    if (payload.category === 'pop') {
       params.genre = 'Pop';
-    } else if (payload.category === 'Rock') {
+    } else if (payload.category === 'rock') {
       params.genre = 'Rock';
-    } else if (payload.category === 'Classical') {
+    } else if (payload.category === 'classical') {
       params.genre = 'Classical';
-    } else if (payload.category === 'Bollywood') {
+    } else if (payload.category === 'bollywood') {
       params.genre = 'Bollywood';
-    } else if (payload.category === 'Hip-Hop') {
+    } else if (payload.category === 'hiphop') {
       params.genre = 'Hip-Hop';
     }
     
